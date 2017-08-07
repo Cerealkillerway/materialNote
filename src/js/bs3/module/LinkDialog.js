ef831dd59eeca9136dcfb56fec65ade7f9b96092
Cereal Killer <cerealkillerway@gmail.com>
2017-08-07 04:57:46 +0200

    fixing link dialog

--- src/js/bs3/module/LinkDialog.js
+++ src/js/bs3/module/LinkDialog.js
@@ -1,163 +1,174 @@
 define([
-  'summernote/base/core/key'
+    'summernote/base/core/key'
 ], function (key) {
-  var LinkDialog = function (context) {
-    var self = this;
-    var ui = $.summernote.ui;
+    var LinkDialog = function (context) {
+        var self = this;
+        var ui = $.summernote.ui;
 
-    var $editor = context.layoutInfo.editor;
-    var options = context.options;
-    var lang = options.langInfo;
+        var $editor = context.layoutInfo.editor;
+        var options = context.options;
+        var lang = options.langInfo;
+        var data;
 
-    this.initialize = function () {
-      var $container = options.dialogsInBody ? $(document.body) : $editor;
+        this.initialize = function () {
+            var $container = options.dialogsInBody ? $(document.body) : $editor;
 
-      var body = '<div class="form-group">' +
-                   '<label>' + lang.link.textToDisplay + '</label>' +
-                   '<input class="note-link-text form-control" type="text" />' +
-                 '</div>' +
-                 '<div class="form-group">' +
-                   '<label>' + lang.link.url + '</label>' +
-                   '<input class="note-link-url form-control" type="text" value="http://" />' +
-                 '</div>' +
-                 (!options.disableLinkTarget ?
-                   '<div class="checkbox">' +
-                     '<label for="sn-checkbox-open-in-new-window">' + 
-                       '<input type="checkbox" id="sn-checkbox-open-in-new-window" checked />' + lang.link.openInNewWindow +
-                     '</label>' +
-                   '</div>' : ''
-                 );
-      var footer = '<button href="#" class="btn btn-primary note-link-btn disabled" disabled>' + lang.link.insert + '</button>';
+            var body = '<div class="form-group">' +
+            '<label>' + lang.link.textToDisplay + '</label>' +
+            '<input class="note-link-text form-control" type="text" />' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label>' + lang.link.url + '</label>' +
+            '<input class="note-link-url form-control" type="text" value="http://" />' +
+            '</div>' +
+            (!options.disableLinkTarget ?
+                '<div class="checkbox">' +
+                '<label for="sn-checkbox-open-in-new-window">' +
+                '<input type="checkbox" id="sn-checkbox-open-in-new-window" checked />' + lang.link.openInNewWindow +
+                '</label>' +
+                '</div>' : ''
+            );
+            var footer = '<button href="#" class="btn btn-primary note-link-btn disabled" disabled>' + lang.link.insert + '</button>';
 
-      this.$dialog = ui.dialog({
-        className: 'link-dialog',
-        title: lang.link.insert,
-        fade: options.dialogsFade,
-        body: body,
-        footer: footer
-      }).render().appendTo($container);
-    };
+            this.$dialog = ui.dialog({
+                className: 'link-dialog',
+                title: lang.link.insert,
+                fade: options.dialogsFade,
+                body: body,
+                footer: footer
+            }).render().appendTo($container);
 
-    this.destroy = function () {
-      ui.hideDialog(this.$dialog);
-      this.$dialog.remove();
-    };
+            this.$dialog.modal({
+                ready: function() {
+                    var $linkText = self.$dialog.find('.note-link-text'),
+                    $linkUrl = self.$dialog.find('.note-link-url'),
+                    $linkBtn = self.$dialog.find('.note-link-btn'),
+                    $openInNewWindow = self.$dialog.find('input[type=checkbox]');
 
-    this.bindEnterKey = function ($input, $btn) {
-      $input.on('keypress', function (event) {
-        if (event.keyCode === key.code.ENTER) {
-          $btn.trigger('click');
-        }
-      });
-    };
+                    context.triggerEvent('dialog.shown');
 
-    /**
-     * toggle update button
-     */
-    this.toggleLinkBtn = function ($linkBtn, $linkText, $linkUrl) {
-      ui.toggleBtn($linkBtn, $linkText.val() && $linkUrl.val());
-    };
+                    // if no url was given, copy text to url
+                    if (!linkInfo.url) {
+                        linkInfo.url = linkInfo.text;
+                    }
 
-    /**
-     * Show link dialog and set event handlers on dialog controls.
-     *
-     * @param {Object} linkInfo
-     * @return {Promise}
-     */
-    this.showLinkDialog = function (linkInfo) {
-      return $.Deferred(function (deferred) {
-        var $linkText = self.$dialog.find('.note-link-text'),
-        $linkUrl = self.$dialog.find('.note-link-url'),
-        $linkBtn = self.$dialog.find('.note-link-btn'),
-        $openInNewWindow = self.$dialog.find('input[type=checkbox]');
+                    $linkText.val(linkInfo.text);
 
-        ui.onDialogShown(self.$dialog, function () {
-          context.triggerEvent('dialog.shown');
+                    var handleLinkTextUpdate = function () {
+                        self.toggleLinkBtn($linkBtn, $linkText, $linkUrl);
+                        // if linktext was modified by keyup,
+                        // stop cloning text from linkUrl
+                        linkInfo.text = $linkText.val();
+                    };
 
-          // if no url was given, copy text to url
-          if (!linkInfo.url) {
-            linkInfo.url = linkInfo.text;
-          }
+                    $linkText.on('input', handleLinkTextUpdate).on('paste', function () {
+                        setTimeout(handleLinkTextUpdate, 0);
+                    });
 
-          $linkText.val(linkInfo.text);
+                    var handleLinkUrlUpdate = function () {
+                        self.toggleLinkBtn($linkBtn, $linkText, $linkUrl);
+                        // display same link on `Text to display` input
+                        // when create a new link
+                        if (!linkInfo.text) {
+                            $linkText.val($linkUrl.val());
+                        }
+                    };
 
-          var handleLinkTextUpdate = function () {
-            self.toggleLinkBtn($linkBtn, $linkText, $linkUrl);
-            // if linktext was modified by keyup,
-            // stop cloning text from linkUrl
-            linkInfo.text = $linkText.val();
-          };
+                    $linkUrl.on('input', handleLinkUrlUpdate).on('paste', function () {
+                        setTimeout(handleLinkUrlUpdate, 0);
+                    }).val(linkInfo.url).trigger('focus');
 
-          $linkText.on('input', handleLinkTextUpdate).on('paste', function () {
-            setTimeout(handleLinkTextUpdate, 0);
-          });
+                    self.toggleLinkBtn($linkBtn, $linkText, $linkUrl);
+                    self.bindEnterKey($linkUrl, $linkBtn);
+                    self.bindEnterKey($linkText, $linkBtn);
 
-          var handleLinkUrlUpdate = function () {
-            self.toggleLinkBtn($linkBtn, $linkText, $linkUrl);
-            // display same link on `Text to display` input
-            // when create a new link
-            if (!linkInfo.text) {
-              $linkText.val($linkUrl.val());
-            }
-          };
+                    var isChecked = linkInfo.isNewWindow !== undefined ?
+                    linkInfo.isNewWindow : context.options.linkTargetBlank;
 
-          $linkUrl.on('input', handleLinkUrlUpdate).on('paste', function () {
-            setTimeout(handleLinkUrlUpdate, 0);
-          }).val(linkInfo.url).trigger('focus');
+                    $openInNewWindow.prop('checked', isChecked);
 
-          self.toggleLinkBtn($linkBtn, $linkText, $linkUrl);
-          self.bindEnterKey($linkUrl, $linkBtn);
-          self.bindEnterKey($linkText, $linkBtn);
+                    $linkBtn.one('click', function (event) {
+                        event.preventDefault();
 
-          var isChecked = linkInfo.isNewWindow !== undefined ?
-            linkInfo.isNewWindow : context.options.linkTargetBlank;
+                        deferred.resolve({
+                            range: linkInfo.range,
+                            url: $linkUrl.val(),
+                            text: $linkText.val(),
+                            isNewWindow: $openInNewWindow.is(':checked')
+                        });
+                        self.$dialog.modal('hide');
+                    });
+                },
+                complete: function() {
 
-          $openInNewWindow.prop('checked', isChecked);
-
-          $linkBtn.one('click', function (event) {
-            event.preventDefault();
-
-            deferred.resolve({
-              range: linkInfo.range,
-              url: $linkUrl.val(),
-              text: $linkText.val(),
-              isNewWindow: $openInNewWindow.is(':checked')
+                }
             });
-            self.$dialog.modal('hide');
-          });
-        });
+        };
 
-        ui.onDialogHidden(self.$dialog, function () {
-          // detach events
-          $linkText.off('input paste keypress');
-          $linkUrl.off('input paste keypress');
-          $linkBtn.off('click');
+        this.destroy = function () {
+            ui.hideDialog(this.$dialog);
+            this.$dialog.remove();
+        };
 
-          if (deferred.state() === 'pending') {
-            deferred.reject();
-          }
-        });
+        this.bindEnterKey = function ($input, $btn) {
+            $input.on('keypress', function (event) {
+                if (event.keyCode === key.code.ENTER) {
+                    $btn.trigger('click');
+                }
+            });
+        };
 
-        ui.showDialog(self.$dialog);
-      }).promise();
+        /**
+        * toggle update button
+        */
+        this.toggleLinkBtn = function ($linkBtn, $linkText, $linkUrl) {
+            ui.toggleBtn($linkBtn, $linkText.val() && $linkUrl.val());
+        };
+
+        /**
+        * Show link dialog and set event handlers on dialog controls.
+        *
+        * @param {Object} linkInfo
+        * @return {Promise}
+        */
+        this.showLinkDialog = function (linkInfo) {
+            return $.Deferred(function (deferred) {
+
+
+                ui.onDialogHidden(self.$dialog, function () {
+                    // detach events
+                    $linkText.off('input paste keypress');
+                    $linkUrl.off('input paste keypress');
+                    $linkBtn.off('click');
+
+                    if (deferred.state() === 'pending') {
+                        deferred.reject();
+                    }
+                });
+
+                ui.showDialog(self.$dialog);
+            }).promise();
+        };
+
+        /**
+        * @param {Object} layoutInfo
+        */
+        this.show = function () {
+            var linkInfo = context.invoke('editor.getLinkInfo');
+            context.invoke('editor.saveRange');
+
+            ui.showDialog(self.$dialog);
+            data = $.Deferred();
+
+            data.then(function (linkInfo) {
+                context.invoke('editor.restoreRange');
+                context.invoke('editor.createLink', linkInfo);
+            }).fail(function () {
+                context.invoke('editor.restoreRange');
+            });
+        };
+        context.memo('help.linkDialog.show', options.langInfo.help['linkDialog.show']);
     };
 
-    /**
-     * @param {Object} layoutInfo
-     */
-    this.show = function () {
-      var linkInfo = context.invoke('editor.getLinkInfo');
-
-      context.invoke('editor.saveRange');
-      this.showLinkDialog(linkInfo).then(function (linkInfo) {
-        context.invoke('editor.restoreRange');
-        context.invoke('editor.createLink', linkInfo);
-      }).fail(function () {
-        context.invoke('editor.restoreRange');
-      });
-    };
-    context.memo('help.linkDialog.show', options.langInfo.help['linkDialog.show']);
-  };
-
-  return LinkDialog;
+    return LinkDialog;
 });
